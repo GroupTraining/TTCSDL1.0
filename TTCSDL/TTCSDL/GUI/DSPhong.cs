ba6680ef9044f3175cbd76178cb16d72f9a151f3
binhbboong <36161471+binhbboong@users.noreply.github.com>
2018-04-12 20:36:49 +0700

    DSPhong
    Anh em xem thế này đc chưa :v:

---
+++ TTCSDL/TTCSDL/GUI/DSPhong.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTCSDL.GUI
+{
+    public partial class DSPhong : Form
+    {
+        public DSPhong()
+        {
+            InitializeComponent();
+            picturePhongdoi.Hide();
+            picturePhongvip.Hide();
+        }
+
+        private void buttonPdon_Click(object sender, EventArgs e)
+        {
+            picturePhongdon.Show();
+            picturePhongdoi.Hide();
+            picturePhongvip.Hide();
+        }
+
+        private void buttonPdoi_Click(object sender, EventArgs e)
+        {
+            picturePhongdon.Hide();
+            picturePhongdoi.Show();
+            picturePhongvip.Hide();
+        }
+
+        private void buttonPvip_Click(object sender, EventArgs e)
+        {
+            picturePhongdon.Hide();
+            picturePhongdoi.Hide();
+            picturePhongvip.Show();
+        }
+    }
+}
